ab80c887c72bb64fb31af706a1944bd6f1287ee5
Mila Jane Espano <espanomilajane@gmail.com>
2026-03-19 23:22:54 +0800

    Lab 4 Problem_3

--- solution.js
+++ solution.js
@@ -1,18 +1,3 @@
-function generateIDs(count){
-    let id=[];
-
-    for(let i = 0; i < count; i++){
-        if(count === 7){
-            if(i === 5){
-                continue;
-            }
-        }
-        id.push(`ID-${i}`);
-    }
-    return id;
-}
-
-console.log(generateIDs(7))
 
 //PROBLEM_3
 function calculateTotal(...numbers){
