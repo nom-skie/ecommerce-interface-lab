c19734e3059794335c3bd8a38605acfd5ec87479
Mila Jane Espano <espanomilajane@gmail.com>
2026-03-19 23:09:46 +0800

    Lab 4 Problem_3

--- solution.js
+++ solution.js
@@ -13,3 +13,14 @@ function generateIDs(count){
 }
 
 console.log(generateIDs(7))
+
+//PROBLEM_3
+function calculateTotal(...numbers){
+    let sum = numbers.reduce((total, current) => total + current, 0)
+
+    return sum;
+}
+
+console.log(calculateTotal(1,2,3,4,5,6,7,8,9));
+
+
