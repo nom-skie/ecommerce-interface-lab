eef2e4e1583a4ca7b6ed623aaebf0d65efa479e1
Mila Jane Espano <espanomilajane@gmail.com>
2026-03-20 01:11:15 +0800

    Lab 4 Problem_4

--- solution.js
+++ solution.js
@@ -1,2 +1,11 @@
 
+//PROBLEM_3
+function calculateTotal(...numbers){
+    let sum = numbers.reduce((total, current) => total + current, 0)
+
+    return sum;
+}
+
+console.log(calculateTotal(1,2,3,4,5,6,7,8,9));
+
 
