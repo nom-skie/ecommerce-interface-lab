025ec50f2cfc76a2ad977cc67b947ebcd0e348c1
Jayson Alastoy <jayson2k03@gmail.com>
2026-03-26 16:37:29 +0800

    fixed Code

--- solution.js
+++ solution.js
@@ -25,6 +25,26 @@ console.log(checkVariable());
 console.log(checkVariable(null));
 
 
+// Problem 2
+
+function generateIDs(count){
+    let id=[];
+
+    for(let i = 0; i < count; i++){
+        if(count === 7){
+            if(i === 5){
+                continue;
+            }
+        }
+        id.push(`ID-${i}`);
+    }
+    return id;
+}
+
+console.log(generateIDs(7));
+
+
+
 //PROBLEM_3
 function calculateTotal(...numbers){
     let sum = numbers.reduce((total, current) => total + current, 0)
